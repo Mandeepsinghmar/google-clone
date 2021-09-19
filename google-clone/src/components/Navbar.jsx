b54e5589c96aabe8225ec47d2cef3e9f81598346
Mandeep Singhmar <msingh8328@gmail.com>
2021-09-19 15:07:34 +0530

    fix: logo height

--- google-clone/src/components/Navbar.jsx
+++ google-clone/src/components/Navbar.jsx
@@ -6,9 +6,9 @@ export const Navbar = ({ setDarkTheme, darkTheme }) => (
   <div className="p-5 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
     <div className="flex justify-between items-center space-x-5 w-screen">
       <Link to="/">
-        <img className="w-32 h-22 " src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="google-logo" />
+        <img className="sm:w-32 w-28 h-18 sm:h-22 " src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="google-logo" />
       </Link>
-      <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl">{darkTheme ? '💡' : '🌙'}</button>
+      <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 bg-white border rounded p-1">{darkTheme ? '💡' : '🌙'}</button>
     </div>
     <Search />
   </div>
