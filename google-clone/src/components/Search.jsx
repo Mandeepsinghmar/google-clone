8e370cfd7b361b2adb6a37e29bb0c2cd4976a4b9
Mandeep Singhmar <msingh8328@gmail.com>
2021-09-20 10:47:34 +0530

    cancel icon

--- google-clone/src/components/Search.jsx
+++ google-clone/src/components/Search.jsx
@@ -17,14 +17,15 @@ export const Search = () => {
   }, [debouncedValue]);
 
   return (
-    <div className="sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
+    <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
       <input
         value={text}
         type="text"
-        className="sm:w-96 w-80 h-10 dark:bg-gray-200  border rounded-full shadow-sm outline-none p-6 text-black  "
-        placeholder="🔍 Search Google or type URL"
+        className="sm:w-96 w-80 h-10 dark:bg-gray-200  border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
+        placeholder="🔎 Search Google or type URL"
         onChange={(e) => setText(e.target.value)}
       />
+      <button type="button" className="absolute top-1.5 right-4 text-2xl text-gray-500 " onClick={() => setText('')}>x</button>
       <Links />
     </div>
   );
